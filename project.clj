63dfaa334daacdf5ade6a7976c8577e55a0ae17e
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2011-03-08 10:16:18 +0100

    Updated project.clj, added incanter as a dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject clj-histogram "0.1.0-SNAPSHOT"
-  :description ""Histogram package for Clojure"
+(defproject clj-histogram "0.0.1-SNAPSHOT"
+  :description "Spallation plotting application"
+  :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure                "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [incanter "1.2.3"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
