e52f04fed1c52472ad51a29300a1960463a1669c
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2011-03-09 09:58:21 +0100

    Updated swank-clojure dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [incanter "1.2.3"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
