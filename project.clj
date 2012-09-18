bd730ce27767e69c06dcba9097eb1ced96752278
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-09-18 12:07:55 +0300

    Use nrepl and exclude incanter-mongodb

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-histogram "0.0.1-SNAPSHOT"
   :description "Spallation plotting application"
   :repositories {"incanter" "http://repo.incanter.org"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [incanter "1.2.3"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+		 [incanter "1.3.0" :exclusions [incanter/incanter-mongodb]]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]]
+)
