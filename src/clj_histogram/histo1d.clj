464d0a52c8c9c677478fc639818487733e16a100
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-10-15 14:49:20 +0300

    Indentation changes

--- src/clj_histogram/histo1d.clj
+++ src/clj_histogram/histo1d.clj
@@ -3,46 +3,46 @@
 (defn bin-search [coll el]
   (let [v (vec coll)]
     (loop [li (int 0)
-	   ri (int (count v))]
+           ri (int (count v))]
       (if (= (get v 0) el)
-	0
-	(let [p (int (/ (- ri li) 2))]
-	  (if (> p 0)
-	    (let [next-p (int (+ li p))
-		  ce (get v next-p)]
-	      (cond
-	       (= ce el) next-p
-	       (> ce el) (recur li next-p)
-	       (< ce el) (recur next-p ri)))))))))
+        0
+        (let [p (int (/ (- ri li) 2))]
+          (if (> p 0)
+            (let [next-p (int (+ li p))
+                  ce (get v next-p)]
+              (cond
+               (= ce el) next-p
+               (> ce el) (recur li next-p)
+               (< ce el) (recur next-p ri)))))))))
 
 (defn in-bin? [bin x]
   (let [low (:xmin bin)
-	high (:xmax bin)]
+        high (:xmax bin)]
     (and (>= x low) (< x high))))
 
 (defn bin-value-to-index [bin-coll x]
   (let [v (vec bin-coll)]
     (loop [lower (int 0)
-	   upper (int (count v))]
+           upper (int (count v))]
       (if (in-bin? (get v 0) x)
-	0
-	(let [pos (int (/ (- upper lower) 2))]
-	  (if (> pos 0)
-	    (let [next-pos (int (+ lower pos))
-		  center (get v next-pos)]
-	      (cond
-	       (in-bin? center x) next-pos
-	       (< x (:xmin center)) (recur lower next-pos)
-	       (>= x (:xmax center)) (recur next-pos upper)))))))))
+        0
+        (let [pos (int (/ (- upper lower) 2))]
+          (if (> pos 0)
+            (let [next-pos (int (+ lower pos))
+                  center (get v next-pos)]
+              (cond
+               (in-bin? center x) next-pos
+               (< x (:xmin center)) (recur lower next-pos)
+               (>= x (:xmax center)) (recur next-pos upper)))))))))
 
 (defn make-bin [xmin xmax]
   {:xmin xmin :xmax xmax})
 
 (defn make-linear-binning [xmin nbins xmax]
   (let [width (/ (- xmax xmin) nbins)
-	bin-fn (fn [n] (make-bin (+ xmin (* n width))
-				 (+ xmin (* (+ n 1) width))))
-	bin-ids (range 0 nbins)]
+        bin-fn (fn [n] (make-bin (+ xmin (* n width))
+                                (+ xmin (* (+ n 1) width))))
+        bin-ids (range 0 nbins)]
     (map bin-fn bin-ids)))
 
 ;;(defstruct histogram :name :binning :contents)
@@ -52,18 +52,18 @@
      (make-histogram name binning x-coll (constantly 1.0)))
   ([name binning x-coll weight-fn]
      (let [nbins (count binning)
-	   bin-grouper (fn [x] (bin-value-to-index binning x))
-	   bin-content-groups (group-by bin-grouper x-coll)]
+           bin-grouper (fn [x] (bin-value-to-index binning x))
+           bin-content-groups (group-by bin-grouper x-coll)]
        (loop [ind 0
-	      h []]
-	 (if (< ind nbins)
-	   (let [bin (nth binning ind)
-		 content (get bin-content-groups ind)
-		 weighted-content (map #(weight-fn bin %) content)
-		 bin-content (apply + weighted-content)
-		 new-bin (conj bin {:content bin-content})]
-	     (recur (+ ind 1) (conj h new-bin)))
-	   {:name name :histo-type :histo-1d :data h})))))
+              h []]
+         (if (< ind nbins)
+           (let [bin (nth binning ind)
+                 content (get bin-content-groups ind)
+                 weighted-content (map #(weight-fn bin %) content)
+                 bin-content (apply + weighted-content)
+                 new-bin (conj bin {:content bin-content})]
+             (recur (+ ind 1) (conj h new-bin)))
+           {:name name :histo-type :histo-1d :data h})))))
 
 (defn histo1d-add [h1 h2]
   (let [data1 (:data h1)
@@ -89,8 +89,7 @@
                     (partition 3 (interleave xmins1 xmaxs1 sum-contents)))})
       {:error-message "Not equal binnings. Can't sum!"})))
 
-
 (defn rndms [n]
   (let [g (java.util.Random.)
-	gfn #(.nextGaussian g)]
+        gfn #(.nextGaussian g)]
     (repeatedly n gfn)))
