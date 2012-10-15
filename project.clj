faad9ae67d7be3585d70ddf7b122de6043ba520d
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-10-15 14:52:07 +0300

    Use midje and lein-midje for testing and bump the version number

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clj-histogram "0.0.1-SNAPSHOT"
-  :description "Spallation plotting application"
+(defproject clj-histogram "0.0.2-SNAPSHOT"
+  :description "Simple histogramming library for Clojure"
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-		 [incanter "1.3.0" :exclusions [incanter/incanter-mongodb]]
+                 [incanter "1.3.0" :exclusions [incanter/incanter-mongodb]]
+                 [midje "1.4.0"]
                  [org.clojure/tools.nrepl "0.2.0-beta9"]]
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
 )
