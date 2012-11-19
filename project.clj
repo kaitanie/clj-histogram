f797e431c3d64b7be684412b1f8ca82f2e10a6d3
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-11-20 01:52:21 +0200

    Use Incanter 1.4.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Simple histogramming library for Clojure"
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [incanter "1.3.0" :exclusions [incanter/incanter-mongodb]]
+                 [incanter "1.4.0" :exclusions [incanter/incanter-mongodb]]
                  [midje "1.4.0"]
                  [org.clojure/tools.nrepl "0.2.0-beta9"]]
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
