b20c2d963afd378e5ffda29434e6a3995b080de3
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-11-20 02:15:21 +0200

    Added project URL and license

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-histogram "0.1.0"
   :description "Simple histogramming library for Clojure"
+  :url "http://gihub.com/kaitanie/clj-histogram"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [incanter "1.4.0" :exclusions [incanter/incanter-mongodb]]
