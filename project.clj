dd4ff1ac9fdd28ff9b6e9e475402bc19f32456af
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-12-16 20:05:53 +0200

    Updated tools.nrepl to 0.2.0-RC1 and incanter to 1.4.1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [incanter "1.4.0" :exclusions [incanter/incanter-mongodb]]
+                 [incanter "1.4.1" :exclusions [incanter/incanter-mongodb]]
                  [midje "1.4.0"]
-                 [org.clojure/tools.nrepl "0.2.0-beta10"]]
+                 [org.clojure/tools.nrepl "0.2.0-RC1"]]
   :plugins [[lein-midje "2.0.1"]])
